9e6ee38c9557313a870a95c485b4eb491b713293
Richard <richardlife101@gmail.com>
2025-09-19 09:41:14 -0400

    changed string

--- Employee.java
+++ Employee.java
@@ -10,7 +10,9 @@ public class Employee{
     private boolean onLeave;
     private double hourlyWage;
     private double unpaidHours;
-
+    private double paySalary;
+    private double pay; 
+    private double x ;
     public Employee(String fullname, int yearOfBirth, double hourlyWage){
         this.fullname = fullname;
         this.yearOfBirth = yearOfBirth;
@@ -32,10 +34,10 @@ public class Employee{
     public boolean canDrive(){
         int age = calculateAge(2025);
         if (age >= 16) {
-            SystemOutPrint (fullname + "can drive");
+            System.out (fullname + "can drive");
         }
         else{
-            systemOutPrint("You have to wait" +(16-age)+ "more years"); 
+            System.out("You have to wait" +(16-age)+ "more years"); 
         
         }
         // to be completed
@@ -46,28 +48,28 @@ public class Employee{
      */
     private double calculatePay(){
         double calculatepay = pay;
-        doublae beforetax = x ;
-        unpaidHours*HourlyWage = x;
-        x*0.7 = pay;
+        double tax = 0.7 ;
+        unpaidHours * hourlyWage = x;
+        x * tax = pay;
         return pay;
     }
-    }
+    
 
     /*
      * Output the payment record and resets unpaid hours
      */
-    public void paySalary(){
-    paysalary = 
-    SystemOutPrint (fullname + "has recieved a wire transfer of"+ calculatePay+ "CAD");
+    public void paySalary(){ 
+    System.out (fullname + "has recieved a wire transfer of"+ pay+ "CAD");
     
-    UnpaidHours =0;
+    unpaidHours =0;
 }
     
     public void setfullname() {
-    fullname = Maryames;
+    name = fullname ;
 }
-    public void getname() {
+    public String getfullname() {
         return fullname;
     }
+}
 
 
